5ab7d149b35578609042bc690c12bdd2c1dc2d2a
Holuwardammy <adeagbosheriffdeenadebayo@gmail.com>
2026-05-08 18:30:52 +0100

    Connected Neon PostgreSQL

--- nuss_webapp/settings.py
+++ nuss_webapp/settings.py
@@ -4,6 +4,7 @@ Django settings for nuss_webapp project.
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,10 +84,9 @@ WSGI_APPLICATION = 'nuss_webapp.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 
